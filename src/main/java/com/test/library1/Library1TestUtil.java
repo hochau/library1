d4a655c54516be9c642c55fabab1d8e63b5920b1
heqiu <964224367@qq.com>
2020-09-07 12:41:51 +0800

    Initial commit

---
+++ src/main/java/com/test/library1/Library1TestUtil.java
@@ -0,0 +1,17 @@
+package com.test.library1;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.test.commonlibrary.CommonTestUtil;
+
+/**
+ */
+public class Library1TestUtil {
+
+    public static void log(){
+        Log.e(CommonTestUtil.class.getName(),"Library1TestUtil");
+    }
+
+
+}
